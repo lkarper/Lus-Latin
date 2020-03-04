5c66479bab9a4895ba4bbcf5fe8724bef50d94e1
Luther Karper <luther.karper@live.com>
2020-03-04 16:38:38 -0500

    Altered formatting

--- app.js
+++ app.js
@@ -4,31 +4,6 @@ const wordSearched = document.querySelector("#wordSearched");
 const results = document.querySelector("#results");
 const results2 = document.querySelector("#results2");
 
-// const searchWord = () => {
-//     let latinWord = inputBox.value;
-//     wordSearched.innerText= latinWord;
-//     $.ajax({
-//         url: "https://glosbe.com/gapi/translate?from=la&dest=eng&format=json&phrase="+latinWord,
-//         dataType: 'jsonp',
-//         success: function(json) {
-//             results.innerText="";
-//             results2.innerText="";
-//             if (json.tuc.length < 1) {
-//                 results.innerText = "Sorry, no results found."
-//             } else {
-//                 if (json.tuc[0].phrase) {
-//                     results.innerText = json.tuc[0].phrase.text;
-//                 }
-//                 if (json.tuc[0].meanings[0].text) {
-//                     results2.innerHTML = json.tuc[0].meanings[0].text;
-//                 }
-//             }
-//         }
-//     });
-// };
-
-
-
 const searchWord = () => {
     for (let entry of data) {
         if (entry.key === inputBox.value) {
