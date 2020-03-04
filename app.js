a8258e256abf51ebda91edccd889f1363c0795a2
Luther Karper <luther.karper@live.com>
2020-03-04 14:53:32 -0500

    Search works for a

--- app.js
+++ app.js
@@ -4,27 +4,38 @@ const wordSearched = document.querySelector("#wordSearched");
 const results = document.querySelector("#results");
 const results2 = document.querySelector("#results2");
 
-const searchWord = () => {
-    let latinWord = inputBox.value;
-    wordSearched.innerText= latinWord;
-    $.ajax({
-        url: "https://glosbe.com/gapi/translate?from=la&dest=eng&format=json&phrase="+latinWord,
-        dataType: 'jsonp',
-        success: function(json) {
-            results.innerText="";
-            results2.innerText="";
-            if (json.tuc.length < 1) {
-                results.innerText = "Sorry, no results found."
-            } else {
-                if (json.tuc[0].phrase) {
-                    results.innerText = json.tuc[0].phrase.text;
-                }
-                if (json.tuc[0].meanings[0].text) {
-                    results2.innerHTML = json.tuc[0].meanings[0].text;
-                }
-            }
-        }
-    });
-};
+// const searchWord = () => {
+//     let latinWord = inputBox.value;
+//     wordSearched.innerText= latinWord;
+//     $.ajax({
+//         url: "https://glosbe.com/gapi/translate?from=la&dest=eng&format=json&phrase="+latinWord,
+//         dataType: 'jsonp',
+//         success: function(json) {
+//             results.innerText="";
+//             results2.innerText="";
+//             if (json.tuc.length < 1) {
+//                 results.innerText = "Sorry, no results found."
+//             } else {
+//                 if (json.tuc[0].phrase) {
+//                     results.innerText = json.tuc[0].phrase.text;
+//                 }
+//                 if (json.tuc[0].meanings[0].text) {
+//                     results2.innerHTML = json.tuc[0].meanings[0].text;
+//                 }
+//             }
+//         }
+//     });
+// };
 
+
+const searchWord = () => {
+    for (let entry of ls_a) {
+        if (entry.key === inputBox.value) {
+            results.innerText = entry.key;
+            results2.innerText = entry.senses;
+        } else {
+            results2.innerText = "Sorry, no results found"
+        }
+    }
+}
 submitButton.addEventListener('click', searchWord);
