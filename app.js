03ae2eb237047351b6da8b0b3807acaed73b3637
Luther Karper <luther.karper@live.com>
2020-03-05 16:28:51 -0500

    replaced for...of loop with the .find method

--- app.js
+++ app.js
@@ -4,17 +4,30 @@ const wordSearched = document.querySelector("#wordSearched");
 const results = document.querySelector("#results");
 const results2 = document.querySelector("#results2");
 
-const searchWord = () => {
-    for (let entry of data) {
-        if (entry.key === inputBox.value) {
-            wordSearched.innerText = entry.key;
-            results2.innerText = entry.senses;
-            break;
-        } else {
-            results2.innerText = "Sorry, no results found"
-        }
+// const searchWord = () => {
+//     for (let entry of data) {
+//         if (entry.key === inputBox.value) {
+//             wordSearched.innerText = entry.key;
+//             results2.innerText = entry.senses;
+//             break;
+//         } else {
+//             wordSearched.innerText = '';
+//             results2.innerText = "Sorry, no results found"
+//         }
+//     }
+// }
+
+function searchWord () {
+    const word = data.find(word => word.key === inputBox.value);
+    if (!word) {
+        wordSearched.innerText = inputBox.value;
+        results.innerText = "Sorry, no results found"
+    } else {
+        wordSearched.innerText = word.key;
+        results.innerText = word.senses;
     }
-}
+};
+
 inputBox.addEventListener('keydown', (event) => {
     if (event.keyCode === 13) {
         searchWord();
